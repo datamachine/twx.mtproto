a4391dac4f8843e4799d5c559444ba31556b4c71
Phillip Lopo <philliplopo@gmail.com>
2015-07-22 23:58:59 -0700

    BoxedType much more functional now

--- twx/mtproto/coretypes.py
+++ twx/mtproto/coretypes.py
@@ -37,12 +37,12 @@ class BareType:
 
     def __new__(cls, *args, **kwargs):
         if cls is BareType:
-            return cls._type_factory(*args, **kwargs)
+            return cls._bare_type_factory(*args, **kwargs)
 
         return cls._make(*args, **kwargs)
 
     @staticmethod
-    def _type_factory(name, number, params, param_types, result_type):
+    def _bare_type_factory(name, number, params, param_types, result_type):
         param_tuple = namedtuple(name, params)
         param_types = param_tuple(*param_types)
 
@@ -86,22 +86,36 @@ class BoxedType:
     _base_boxed_types = {}
     _boxed_types = {}
 
-    def __new__(cls, bare_obj):
-        boxed_type = cls._get_boxed_type(type(bare_obj))
+    def __new__(cls, *args, **kwargs):
+        if cls is BoxedType:
+            return cls._get_base_boxed_type(*args, **kwargs)
+        elif not isinstance(cls, tuple):
+            return cls._get_boxed_type(*args, **kwargs)
+        else:
+            return cls.new(*args, **kwargs)
+        # boxed_type = cls._get_boxed_type(type(bare_obj))
 
-        return tuple.__new__(boxed_type, bare_obj)
+    @classmethod
+    def new(cls, iterable):
+        return tuple.__new__(cls, iterable)
 
-    @staticmethod
-    def new(name, result_type):
-        return BoxedType._boxed_base_type_factory(name, result_type)
+    @classmethod
+    def _get_base_boxed_type(cls, name, base_type):
+        base_boxed_type = cls._base_boxed_types.get(name)
+        if base_boxed_type is None:
+            base_boxed_type = cls._base_boxed_types.setdefault(name, cls._boxed_base_type_factory(name, base_type))
+        return base_boxed_type
 
-    @staticmethod
-    def _boxed_base_type_factory(name, result_type):
-        return BoxedType._base_boxed_types.setdefault(name, type(name, (BoxedType, result_type,), dict(name=name)))
+    @classmethod
+    def _boxed_base_type_factory(cls, name, base_type):
+        return type(name, (cls, base_type,), dict(name=name))
 
     @classmethod
     def _get_boxed_type(cls, bare_type):
-        return cls._boxed_types.setdefault(bare_type.number, cls._boxed_type_factory(bare_type))
+        boxed_type = cls._boxed_types.get(bare_type)
+        if boxed_type is None:
+            boxed_type = cls._boxed_types.setdefault(bare_type, cls._boxed_type_factory(bare_type))
+        return boxed_type
 
     @classmethod
     def _boxed_type_factory(cls, bare_type):
@@ -239,28 +253,36 @@ class vector_c(namedtuple('vector_c', 't num items'), BareType):
 
     def __new__(cls, *args, **kwargs):
         if cls is vector_c:
-            return cls._type_factory(*args, **kwargs)
-        print('making ', cls, args)
-        return cls._make(*args, **kwargs)
+            return cls.get_vector_type(*args, **kwargs)
+        print('__new__:', cls, *args, **kwargs)
+        return cls.new(*args, **kwargs)
 
     @classmethod
+    def new(cls, items):
+        return tuple.__new__(cls, (cls._item_type, len(items), tuple(items),))
+
+    @classmethod
+    def get_vector_type(cls, item_type):
+        key = (cls.__name__, item_type.__name__,)
+        
+        result = cls._vector_types.get(key)
+        if result is None:
+            result = cls._vector_types.setdefault(key, vector_c._type_factory(cls, item_type))
+
+        return result
+
+    @staticmethod
     def _type_factory(cls, item_type):
-        key = (cls, item_type,)
+        attrs = dict(
+            _item_type=item_type
+            )
 
-        def new_vector_type(name, item_type):
+        return type(cls.__name__, (cls,), attrs)
 
-            attrs = dict(
-                __new__=partialmethod(cls.__new__, item_type)
-                )
-
-            return type(name, (cls,), attrs)
-
-        return cls._vector_types.setdefault(key, new_vector_type(cls.__name__, item_type))
-
-    @classmethod
-    def _make(cls, t, iterable):
-        items = [t(i) for i in iterable]
-        return super()._make((t, int_c(len(items)), items,))
+    # @classmethod
+    # def _make(cls, t, iterable):
+    #     items = [t(i) for i in iterable]
+    #     return super()._make((t, int_c(len(items)), items,))
 
     def buffers(self):
         bufs = int_c.buffers(len(self.items))
@@ -273,9 +295,9 @@ class bytes_c(string_c):
     pass
 
 
-class Vector(BoxedType, vector_c):
-
+class Vector(vector_c, BoxedType):
+        
     def __new__(cls, *args, **kwargs):
         if cls is Vector:
-            return cls._type_factory(*args, **kwargs)
-        return vector_c.__new__(cls, *args, **kwargs)
+            return cls.get_vector_type(*args, **kwargs)
+        return cls.new(*args, **kwargs)
--- twx/mtproto/scheme.py
+++ twx/mtproto/scheme.py
@@ -14,7 +14,7 @@ resPQ_c = BareType(name='resPQ_c', number=0x05162463,
     param_types=[int128_c, int128_c, bytes_c, Vector(long_c)],
     result_type=ResPQ_t)
 
-ResPQ = BoxedType.new('ResPQ', ResPQ_t)
+ResPQ = BoxedType('ResPQ', ResPQ_t)
 
 """
 p_q_inner_data#83c95aec pq:bytes p:bytes q:bytes nonce:int128 server_nonce:int128 new_nonce:int256 = P_Q_inner_data
@@ -27,7 +27,7 @@ p_q_inner_data_c = BareType(name='p_q_inner_data_c', number=0x83c95aec,
     param_types=[bytes_c, bytes_c, bytes_c, int128_c, int128_c, int256_c],
     result_type=P_Q_inner_data_t)
 
-P_Q_inner_data = BoxedType.new('P_Q_inner_data', P_Q_inner_data_t)
+P_Q_inner_data = BoxedType('P_Q_inner_data', P_Q_inner_data_t)
 
 """
 server_DH_params_fail#79cb045d nonce:int128 server_nonce:int128 new_nonce_hash:int128 = Server_DH_Params
@@ -46,7 +46,7 @@ server_DH_params_ok_c = BareType(name='server_DH_params_ok_c', number=0xd0e8075c
     param_types=[int128_c, int128_c, bytes_c],
     result_type=Server_DH_Params_t)
 
-Server_DH_Params = BoxedType.new('Server_DH_Params', Server_DH_Params_t)
+Server_DH_Params = BoxedType('Server_DH_Params', Server_DH_Params_t)
 
 """
 server_DH_inner_data#b5890dba nonce:int128 server_nonce:int128 g:int dh_prime:bytes g_a:bytes server_time:int = Server_DH_inner_data
@@ -59,7 +59,7 @@ server_DH_inner_data_c = BareType(name='server_DH_inner_data_c', number=0xb5890d
     param_types=[int128_c, int128_c, int_c, bytes_c, bytes_c, int_c],
     result_type=Server_DH_inner_data_t)
 
-Server_DH_inner_data = BoxedType.new('Server_DH_inner_data', Server_DH_inner_data_t)
+Server_DH_inner_data = BoxedType('Server_DH_inner_data', Server_DH_inner_data_t)
 
 """
 client_DH_inner_data#6643b654 nonce:int128 server_nonce:int128 retry_id:long g_b:bytes = Client_DH_Inner_Data
@@ -72,7 +72,7 @@ client_DH_inner_data_c = BareType(name='client_DH_inner_data_c', number=0x6643b6
     param_types=[int128_c, int128_c, long_c, bytes_c],
     result_type=Client_DH_Inner_Data_t)
 
-Client_DH_Inner_Data = BoxedType.new('Client_DH_Inner_Data', Client_DH_Inner_Data_t)
+Client_DH_Inner_Data = BoxedType('Client_DH_Inner_Data', Client_DH_Inner_Data_t)
 
 """
 dh_gen_ok#3bcbf734 nonce:int128 server_nonce:int128 new_nonce_hash1:int128 = Set_client_DH_params_answer
@@ -97,7 +97,7 @@ dh_gen_fail_c = BareType(name='dh_gen_fail_c', number=0xa69dae02,
     param_types=[int128_c, int128_c, int128_c],
     result_type=Set_client_DH_params_answer_t)
 
-Set_client_DH_params_answer = BoxedType.new('Set_client_DH_params_answer', Set_client_DH_params_answer_t)
+Set_client_DH_params_answer = BoxedType('Set_client_DH_params_answer', Set_client_DH_params_answer_t)
 
 """
 rpc_result#f35c6d01 req_msg_id:long result:Object = RpcResult
@@ -110,7 +110,7 @@ rpc_result_c = BareType(name='rpc_result_c', number=0xf35c6d01,
     param_types=[long_c, object],
     result_type=RpcResult_t)
 
-RpcResult = BoxedType.new('RpcResult', RpcResult_t)
+RpcResult = BoxedType('RpcResult', RpcResult_t)
 
 """
 rpc_error#2144ca19 error_code:int error_message:string = RpcError
@@ -123,7 +123,7 @@ rpc_error_c = BareType(name='rpc_error_c', number=0x2144ca19,
     param_types=[int_c, string_c],
     result_type=RpcError_t)
 
-RpcError = BoxedType.new('RpcError', RpcError_t)
+RpcError = BoxedType('RpcError', RpcError_t)
 
 """
 rpc_answer_unknown#5e2ad36e = RpcDropAnswer
@@ -148,7 +148,7 @@ rpc_answer_dropped_c = BareType(name='rpc_answer_dropped_c', number=0xa43ad8b7,
     param_types=[long_c, int_c, int_c],
     result_type=RpcDropAnswer_t)
 
-RpcDropAnswer = BoxedType.new('RpcDropAnswer', RpcDropAnswer_t)
+RpcDropAnswer = BoxedType('RpcDropAnswer', RpcDropAnswer_t)
 
 """
 future_salt#0949d9dc valid_since:int valid_until:int salt:long = FutureSalt
@@ -161,7 +161,7 @@ future_salt_c = BareType(name='future_salt_c', number=0x0949d9dc,
     param_types=[int_c, int_c, long_c],
     result_type=FutureSalt_t)
 
-FutureSalt = BoxedType.new('FutureSalt', FutureSalt_t)
+FutureSalt = BoxedType('FutureSalt', FutureSalt_t)
 
 """
 future_salts#ae500895 req_msg_id:long now:int salts:vector<future_salt> = FutureSalts
@@ -174,7 +174,7 @@ future_salts_c = BareType(name='future_salts_c', number=0xae500895,
     param_types=[long_c, int_c, vector_c(future_salt_c)],
     result_type=FutureSalts_t)
 
-FutureSalts = BoxedType.new('FutureSalts', FutureSalts_t)
+FutureSalts = BoxedType('FutureSalts', FutureSalts_t)
 
 """
 pong#347773c5 msg_id:long ping_id:long = Pong
@@ -187,7 +187,7 @@ pong_c = BareType(name='pong_c', number=0x347773c5,
     param_types=[long_c, long_c],
     result_type=Pong_t)
 
-Pong = BoxedType.new('Pong', Pong_t)
+Pong = BoxedType('Pong', Pong_t)
 
 """
 destroy_session_ok#e22045fc session_id:long = DestroySessionRes
@@ -206,7 +206,7 @@ destroy_session_none_c = BareType(name='destroy_session_none_c', number=0x62d350
     param_types=[long_c],
     result_type=DestroySessionRes_t)
 
-DestroySessionRes = BoxedType.new('DestroySessionRes', DestroySessionRes_t)
+DestroySessionRes = BoxedType('DestroySessionRes', DestroySessionRes_t)
 
 """
 new_session_created#9ec20908 first_msg_id:long unique_id:long server_salt:long = NewSession
@@ -219,7 +219,7 @@ new_session_created_c = BareType(name='new_session_created_c', number=0x9ec20908
     param_types=[long_c, long_c, long_c],
     result_type=NewSession_t)
 
-NewSession = BoxedType.new('NewSession', NewSession_t)
+NewSession = BoxedType('NewSession', NewSession_t)
 
 """
 message msg_id:long seqno:int bytes:int body:Object = Message
@@ -232,7 +232,7 @@ message_c = BareType(name='message_c', number=crc32('message msg_id:long seqno:i
     param_types=[long_c, int_c, int_c, object],
     result_type=Message_t)
 
-Message = BoxedType.new('Message', Message_t)
+Message = BoxedType('Message', Message_t)
 
 """
 msg_container#73f1f8dc messages:vector<%Message> = MessageContainer
@@ -245,7 +245,7 @@ msg_container_c = BareType(name='msg_container_c', number=0x73f1f8dc,
     param_types=[vector_c(message_c)],
     result_type=MessageContainer_t)
 
-MessageContainer = BoxedType.new('MessageContainer', MessageContainer_t)
+MessageContainer = BoxedType('MessageContainer', MessageContainer_t)
 
 """
 msg_copy#e06046b2 orig_message:Message = MessageCopy
@@ -258,7 +258,7 @@ msg_copy_c = BareType(name='msg_copy_c', number=0xe06046b2,
     param_types=[Message],
     result_type=MessageCopy_t)
 
-MessageCopy = BoxedType.new('MessageCopy', MessageCopy_t)
+MessageCopy = BoxedType('MessageCopy', MessageCopy_t)
 
 """
 gzip_packed#3072cfa1 packed_data:bytes = Object
@@ -271,7 +271,7 @@ gzip_packed_c = BareType(name='gzip_packed_c', number=0x3072cfa1,
     param_types=[bytes_c],
     result_type=TLType_t)
 
-TLType = BoxedType.new('TLType', TLType_t)
+TLType = BoxedType('TLType', TLType_t)
 
 """
 msgs_ack#62d6b459 msg_ids:Vector<long> = MsgsAck
@@ -284,7 +284,7 @@ msgs_ack_c = BareType(name='msgs_ack_c', number=0x62d6b459,
     param_types=[Vector(long_c)],
     result_type=MsgsAck_t)
 
-MsgsAck = BoxedType.new('MsgsAck', MsgsAck_t)
+MsgsAck = BoxedType('MsgsAck', MsgsAck_t)
 
 """
 bad_msg_notification#a7eff811 bad_msg_id:long bad_msg_seqno:int error_code:int = BadMsgNotification
@@ -303,7 +303,7 @@ bad_server_salt_c = BareType(name='bad_server_salt_c', number=0xedab447b,
     param_types=[long_c, int_c, int_c, long_c],
     result_type=BadMsgNotification_t)
 
-BadMsgNotification = BoxedType.new('BadMsgNotification', BadMsgNotification_t)
+BadMsgNotification = BoxedType('BadMsgNotification', BadMsgNotification_t)
 
 """
 msg_resend_req#7d861a08 msg_ids:Vector<long> = MsgResendReq
@@ -316,7 +316,7 @@ msg_resend_req_c = BareType(name='msg_resend_req_c', number=0x7d861a08,
     param_types=[Vector(long_c)],
     result_type=MsgResendReq_t)
 
-MsgResendReq = BoxedType.new('MsgResendReq', MsgResendReq_t)
+MsgResendReq = BoxedType('MsgResendReq', MsgResendReq_t)
 
 """
 msgs_state_req#da69fb52 msg_ids:Vector<long> = MsgsStateReq
@@ -329,7 +329,7 @@ msgs_state_req_c = BareType(name='msgs_state_req_c', number=0xda69fb52,
     param_types=[Vector(long_c)],
     result_type=MsgsStateReq_t)
 
-MsgsStateReq = BoxedType.new('MsgsStateReq', MsgsStateReq_t)
+MsgsStateReq = BoxedType('MsgsStateReq', MsgsStateReq_t)
 
 """
 msgs_state_info#04deb57d req_msg_id:long info:bytes = MsgsStateInfo
@@ -342,7 +342,7 @@ msgs_state_info_c = BareType(name='msgs_state_info_c', number=0x04deb57d,
     param_types=[long_c, bytes_c],
     result_type=MsgsStateInfo_t)
 
-MsgsStateInfo = BoxedType.new('MsgsStateInfo', MsgsStateInfo_t)
+MsgsStateInfo = BoxedType('MsgsStateInfo', MsgsStateInfo_t)
 
 """
 msgs_all_info#8cc0d131 msg_ids:Vector<long> info:bytes = MsgsAllInfo
@@ -355,7 +355,7 @@ msgs_all_info_c = BareType(name='msgs_all_info_c', number=0x8cc0d131,
     param_types=[Vector(long_c), bytes_c],
     result_type=MsgsAllInfo_t)
 
-MsgsAllInfo = BoxedType.new('MsgsAllInfo', MsgsAllInfo_t)
+MsgsAllInfo = BoxedType('MsgsAllInfo', MsgsAllInfo_t)
 
 """
 msg_detailed_info#276d3ec6 msg_id:long answer_msg_id:long bytes:int status:int = MsgDetailedInfo
@@ -368,7 +368,7 @@ msg_detailed_info_c = BareType(name='msg_detailed_info_c', number=0x276d3ec6,
     param_types=[long_c, long_c, int_c, int_c],
     result_type=MsgDetailedInfo_t)
 
-MsgDetailedInfo = BoxedType.new('MsgDetailedInfo', MsgDetailedInfo_t)
+MsgDetailedInfo = BoxedType('MsgDetailedInfo', MsgDetailedInfo_t)
 
 if __name__ == '__main__':
     res_pq = resPQ_c(1, 2, b'test1', [10, 20, 30, 40, 50])
